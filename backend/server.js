d96234e4ceff6c02ef87eba15c998b6210c9f50a
theodanis <51154027+theodanis@users.noreply.github.com>
2025-03-16 23:21:51 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,26 @@
 const express = require('express');
+const cors = require('cors');  // CORS paketini dahil ediyoruz
 const axios = require('axios');
-const cors = require('cors'); // CORS middleware
 
 const app = express();
-const port = process.env.PORT || 3000; // Render’a uygun port
 
-// CORS middleware: Sadece guesstock.com'dan gelen isteklere izin veriyoruz
-app.use(cors({
-  origin: 'https://www.guesstock.com'
-}));
+// CORS'u aktif hale getiriyoruz
+app.use(cors());  // Bu, tüm kaynaklardan gelen istekleri kabul eder
 
-// API route'u
-app.get('/api/stock-data/:stockSymbol', async (req, res) => {
-  const { stockSymbol } = req.params; // Stock symbol'ı URL parametresinden alıyoruz
+const port = process.env.PORT || 5000;
 
+app.get('/api/stock-data/:symbol', async (req, res) => {
+  const stockSymbol = req.params.symbol;
   try {
     const response = await axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${stockSymbol}?interval=1d&range=5y`);
     const data = response.data;
-    res.json(data); // API'den aldığımız veriyi frontend'e gönderiyoruz
+    res.json(data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Veri çekme hatası' });
+    res.status(500).send('Error fetching stock data');
   }
 });
 
-// Sunucu başlatma
 app.listen(port, () => {
-  console.log(`Sunucu http://localhost:${port} adresinde çalışıyor.`);
+  console.log(`Server running on port ${port}`);
 });
